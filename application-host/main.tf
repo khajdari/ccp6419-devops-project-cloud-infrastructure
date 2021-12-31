5f60a284ce51f9cb162a9f570f308362a55d8ad9
Kristian Hajdari <khajdari@athtech.gr>
2021-12-31 20:18:06 +0200

    fix

--- application-host/main.tf
+++ application-host/main.tf
@@ -1,6 +1,9 @@
 provider "azurerm" {
   features {}
   version = "=2.37.0"
+
+  subscription_id = "01c3b346-bca0-4aca-9098-835267f0e8aa"
+  tenant_id       = "979c83c1-112c-4add-812a-d3ea1a9e2886"
 }
 
 # create a resource group
