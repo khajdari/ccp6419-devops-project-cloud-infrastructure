2f91ffd05fd75fbdfad6bcc6af4040afbe85f578
Kristian Hajdari <khajdari@athtech.gr>
2022-01-02 05:47:18 +0200

    test

--- configuration-server/main.tf
+++ configuration-server/main.tf
@@ -3,7 +3,7 @@ provider "azurerm" {
   version = "=2.46.0"
 }
 
-# create a resource group for the configuration server
+# create a resource group for the configuration manager server
 resource "azurerm_resource_group" "ccp6419-configuration-server" {
   name     = "ccp6419-configuration-server"
   location = var.location
