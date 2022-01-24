85c4055cefbcaaae441d3fd865e4b639c6db0157
Kristian Hajdari <khajdari@athtech.gr>
2022-01-24 20:48:05 +0200

    ccp6419: initialize azure infrastructure

--- automation-server/ccp6419-automation-server-setup.sh
+++ automation-server/ccp6419-automation-server-setup.sh
@@ -47,6 +47,7 @@ sudo apt-get update && sudo apt-get install -y gnupg software-properties-common 
 curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
 sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
 sudo apt-get update && sudo apt-get install terraform
+sudo apt install sshpass
 
 # setup ssh availability
 sudo sed -i "/^[^#]*PasswordAuthentication[[:space:]]no/c\PasswordAuthentication yes" /etc/ssh/sshd_config
